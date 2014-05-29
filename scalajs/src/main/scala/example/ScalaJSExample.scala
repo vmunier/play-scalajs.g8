c4b48dbe0a9a5fe348cca63cd61e92305e1437b3
Vincent Munier <vmunier.fr@gmail.com>
2014-05-29 12:05:44 +0100

    Upgrade to Scala.js v0.5.0-RC1

--- scalajs/src/main/scala/example/ScalaJSExample.scala
+++ scalajs/src/main/scala/example/ScalaJSExample.scala
@@ -3,11 +3,8 @@ package example
 import scala.scalajs.js
 import js.Dynamic.{ global => g }
 import shared.SharedMessages
-import js.annotation.JSExport
 
-@JSExport
-object ScalaJSExample {
-  @JSExport
+object ScalaJSExample extends js.JSApp {
   def main(): Unit = {
     g.document.getElementById("scalajsShoutOut").textContent = SharedMessages.itWorks
   }
