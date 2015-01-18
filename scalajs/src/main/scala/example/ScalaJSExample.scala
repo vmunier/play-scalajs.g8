1c6bc00afa2b57808df4e01c9e5d23a4b1e8beda
Vincent Munier <vmunier.fr@gmail.com>
2015-01-18 01:03:46 +0000

    Use the scalajs dom library

--- scalajs/src/main/scala/example/ScalaJSExample.scala
+++ scalajs/src/main/scala/example/ScalaJSExample.scala
@@ -1,12 +1,12 @@
 package example
 
 import scala.scalajs.js
-import js.Dynamic.{ global => g }
+import org.scalajs.dom
 import shared.SharedMessages
 
 object ScalaJSExample extends js.JSApp {
   def main(): Unit = {
-    g.document.getElementById("scalajsShoutOut").textContent = SharedMessages.itWorks
+    dom.document.getElementById("scalajsShoutOut").textContent = SharedMessages.itWorks
   }
 
   /** Computes the square of an integer.
